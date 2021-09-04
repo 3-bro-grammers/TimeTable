19de47ec056187db4c8b797ccc74193e18fa5b79
sundar-2000 <sundarlinga6@gmail.com>
2021-09-04 22:45:15 +0530

    added fetch logic

--- public/script.js
+++ public/script.js
@@ -1,6 +1,21 @@
-function get_schedule_clk(){
-    console.log(1)
-    fetch("schedule.json").then(res=>{
-        res.json().then(data=>console.log(data))
-    })
+function get_schedule_clk() {
+  var url = "schedule.json";
+  var today=new Date();
+  var tmrw=new Date();
+  tmrw.setDate(tmrw.getDate() + 1);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.table(data);
+    });
+    var today_key = `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()}`;
+    var tmrw_key=`${tmrw.getDate()}.${tmrw.getMonth()+1}.${tmrw.getFullYear()}`;
+    var today_res=[],tmrw_res=[];
+    if(today.getDay()==0)
+    {
+        today_res[0]="S";
+        tmrw_res="Please check schedule";
+    }
+    if(!data[tmrw_key])
+    tmrw_res="";
 }
