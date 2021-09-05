822141050472d2fdaa7514f5c76a1e5546a8fbd3
agilan2001 <agilanvlr2001@gmail.com>
2021-09-05 08:18:59 +0530

    schedule logic done

--- public/script.js
+++ public/script.js
@@ -1,21 +1,49 @@
 function get_schedule_clk() {
-  var url = "schedule.json";
-  var today=new Date();
-  var tmrw=new Date();
-  tmrw.setDate(tmrw.getDate() + 1);
-  fetch(url)
+  if (sel_optical.value == sel_wireless.value || sel_miniproj.value == sel_optical.value || sel_miniproj.value == sel_wireless.value) {
+    console.log("Invalid Combination");
+    return;
+  }
+  var staff_select = { w: [], x: [], y: [] };
+  staff_select[sel_optical.value] = ["Optical Lab", check_optical.checked];
+  staff_select[sel_wireless.value] = ["Wireless Lab", check_wireless.checked];
+  staff_select[sel_miniproj.value] = ["Mini-Project", check_miniproj.checked];
+  console.log(staff_select)
+
+  var today = [new Date()];
+  var tmrw = [new Date()];
+  tmrw[0].setDate(tmrw[0].getDate() + 1);
+  today[1] = `${today[0].getDate()}.${today[0].getMonth() + 1}.${today[0].getFullYear()}`;
+  tmrw[1] = `${tmrw[0].getDate()}.${tmrw[0].getMonth() + 1}.${tmrw[0].getFullYear()}`;
+
+  fetch("schedule.json",{cache: "no-store"})
     .then((res) => res.json())
     .then((data) => {
-      console.table(data);
+      // console.table(data);
+      var classes=[];
+
+      
+      [today,tmrw].forEach((e,i)=>{
+        if(e[0].getDay() == 0){
+          classes[i] = ["Sunday", "Sunday"];
+        }else{
+          classes[i] = [ (!staff_select[data[e[1]][0]][1] || (sel_wireless.value!="x")) ? staff_select[data[e[1]][0]][0] : "Free Now",
+                         (staff_select[data[e[1]][1]][1] || (sel_wireless.value!="x"))  ? staff_select[data[e[1]][1]][0] : "Free Now" ];
+        }
+      });
+    
+
+      sched_body.innerHTML=`
+      <tr>
+        <td>${today[1]}</td>
+        <td>${classes[0][0]}</td>
+        <td>${classes[0][1]}</td>
+      </tr>
+      <tr>
+      <td>${tmrw[1]}</td>
+      <td>${classes[1][0]}</td>
+      <td>${classes[1][1]}</td>
+      </tr>
+      `
     });
-    var today_key = `${today.getDate()}.${today.getMonth()+1}.${today.getFullYear()}`;
-    var tmrw_key=`${tmrw.getDate()}.${tmrw.getMonth()+1}.${tmrw.getFullYear()}`;
-    var today_res=[],tmrw_res=[];
-    if(today.getDay()==0)
-    {
-        today_res[0]="S";
-        tmrw_res="Please check schedule";
-    }
-    if(!data[tmrw_key])
-    tmrw_res="";
+
 }
